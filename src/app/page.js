b1fcbed09912312f1977ad3720e4a0d21d94cb50
doleduc2710 <doleduc2710@gmail.com>
2024-05-16 21:29:53 +0700

    Init project

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Hello world by Do Le Duc ^^ &nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
